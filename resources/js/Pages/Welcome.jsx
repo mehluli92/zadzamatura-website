8f44ffc97a5a6edc8aa0fafee1a86d4f23847580
mehluli92 <nokwaramehluli@gmail.com>
2024-11-27 21:16:21 +0200

    Initial commit

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,46 @@
+import { Head, Link } from '@inertiajs/react';
+
+export default function Welcome({ auth}) {
+
+    return (
+        <>
+            <Head title="Home" />
+            <div className="">
+                <div className="relative flex min-h-screen flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
+                    <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+
+                        <main className="mt-6 h-[600px]">
+                            
+                        </main>
+
+                        <footer className="py-16 text-center text-sm text-black dark:text-white/70">
+                        {auth.user ? (
+                                    <Link
+                                        href={route('dashboard')}
+                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Dashboard
+                                    </Link>
+                                ) : (
+                                    <>
+                                        <Link
+                                            href={route('login')}
+                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        >
+                                            Log in
+                                        </Link>
+                                        <Link
+                                            href={route('register')}
+                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        >
+                                            Register
+                                        </Link>
+                                    </>
+                                )}
+                        </footer>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
